fb80e24e311772de1afcfac1b11f5699360fd3ab
Amal <amalprakash2025@gmail.com>
2022-11-22 00:04:00 +0530

    Second Commit

---
+++ Work-out-3/Program-1/Main.java
@@ -0,0 +1,10 @@
+// Program-1
+// Create an abstract class OutdoorGame inside the package ‘sports’. Inherit it for Football and 
+// Cricket. Create another package ‘sportsstar’. Inside which create two class Footballer and 
+// Cricketer. Each class shows individual income based on the rating ( where rating is assigned 
+// based on different bowling or bating avg or total goal done or saved by different sports persons) 
+// of its different star sports person.
+
+public class Main {
+    
+}
